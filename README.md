31a728db88bab704f5692bee68a165307448415c
Carlos Leonardo dos S. Mendes <leo@caleo.com.br>
2021-08-21 17:24:16 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ethereum Marketplace
+
+Mercado Blockchain baseado no tutorial do canal Dapp University (http://www.dappuniversity.com) 
+YouTube tutorial: https://www.youtube.com/watch?v=VH9Q2lf2mNo
---
+++ src/components/Main.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+
+  render() {
+    return (
+      <div id="content">
+        <h1>Adicionar Produto</h1>
+        <form onSubmit={(event) => {
+          event.preventDefault()
+          const name = this.productName.value
+          const price = window.web3.utils.toWei(this.productPrice.value.toString(), 'Ether')
+          this.props.createProduct(name, price)
+        }}>
+          <div className="form-group mr-sm-2">
+            <input
+              id="productName"
+              type="text"
+              ref={(input) => { this.productName = input }}
+              className="form-control"
+              placeholder="Nome do produto"
+              required />
+          </div>
+          <div className="form-group mr-sm-2">
+            <input
+              id="productPrice"
+              type="text"
+              ref={(input) => { this.productPrice = input }}
+              className="form-control"
+              placeholder="Preço do produto (em Ether)"
+              required />
+          </div>
+          <button type="submit" className="btn btn-primary">Adicionar produto</button>
+        </form>
+        <p>&nbsp;</p>
+        <h2>Comprar produto</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Nome</th>
+              <th scope="col">Preço</th>
+              <th scope="col">Proprietário</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody id="productList">
+            {this.props.products.map((product, key) => {
+              return (
+                <tr key={key}>
+                  <th scope="row">{product.id.toString()}</th>
+                  <td>{product.name}</td>
+                  <td>{window.web3.utils.fromWei(product.price.toString(), "ether")} Eth</td>
+                  <td>{product.owner}</td>
+                  <td>
+                    {!product.purchased
+                      ? <button
+                        name={product.id}
+                        value={product.price}
+                        onClick={(event) => {
+                          this.props.purchaseProduct(event.target.name, event.target.value)
+                        }}
+                        >
+                          Comprar
+                        </button>
+                      : null
+                    }
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+        <p><a href="https://ropsten.etherscan.io/address/0x781c71bfe45d1c5d81fca75d17bb589cc72d63fc" target="_blank">Informação do contrato</a></p>
+      </div>
+    );
+  }
+}
+
+export default Main;
